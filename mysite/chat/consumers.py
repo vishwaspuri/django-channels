dd9d7769dbb0c4d4f82e2d80727c07afb16f5a9d
vishwaspuri <vishwaspuri09@gmail.com>
2019-06-17 22:45:38 +0530

    Added asynchronous chat server

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -1,18 +1,42 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-    
-    def disconnect(self, close_code):
-        pass
-    
-    def receive(self, text_data):
-        text_data_json=json.load(text_data)
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name=self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name='chat_%s'%self.room_name
+
+    #join room group
+        await self.channel_layer.group_add(
+        self.room_group_name,
+        self.channel_name
+        )
+
+        await self.accept()
+
+
+    async def disconnect(self, close_code):
+    #leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+            )
+
+    async def receive(self, text_data):
+        text_data_json=json.loads(text_data)
         message=text_data_json['message']
+        #Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'message':message
+            }
+        )
 
-        self.send(text_data=json.dumps({
+    async def chat_message(self, event):
+        message=event['message'] 
+
+        await self.send(text_data=json.dumps({
             'message':message
-        }))
-
+        })) 
